f168f0c8f23403939a32cb19ea52561e7b63d862
ChengYeh Wang <cywang.eda@gmail.com>
2021-03-23 10:29:48 +0800

    sync with git server

---
+++ Shared/webkit_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  webkit_testApp.swift
+//  Shared
+//
+//  Created by cywang on 2021/3/23.
+//
+
+import SwiftUI
+
+@main
+struct webkit_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
